56a20ba75810535fcd0759815fa352be61cf6777
Warunee Benjanan <warunee.be@ku.th>
2022-06-27 13:15:22 +0700

    Update Cat and add Dog

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -3,5 +3,6 @@ package myapp;
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Hello, Posie");
+        System.out.println("Nice to meet you");
     }
 }
