40cc17f0139fd2ed21ce743600f2332e089f5455
Warunee Benjanan <warunee.be@ku.th>
2022-06-27 13:49:31 +0700

    bring back src/myapp/Cat.java from commit 8958294

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -3,6 +3,5 @@ package myapp;
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Hello, Posie");
-        System.out.println("Nice to meet you");
     }
 }
